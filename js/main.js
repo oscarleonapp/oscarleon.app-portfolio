5b35195a24831268fef7ca1bb37b9b4348594ad5
oscardev-fe <oscarleon.alcatel@gmail.com>
2023-03-23 18:38:04 -0600

    al darle click ocultar menu

--- js/main.js
+++ js/main.js
@@ -31,4 +31,8 @@ window.onscroll = () =>{
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrolloY > 100);
+
+    // Remove toggle and navbar
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
