33e8ad079e687f0d3934aac8e7f85aa80c140f4a
oscardev-fe <oscarleon.alcatel@gmail.com>
2023-03-23 19:03:05 -0600

    scrollveal

--- js/main.js
+++ js/main.js
@@ -36,3 +36,13 @@ window.onscroll = () =>{
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+// Scroll reveal
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
