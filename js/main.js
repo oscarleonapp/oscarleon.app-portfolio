9438411fb08a896f251a7d9e0cbb1938c4a2bc1e
Oscar León <oscarleon.alcatel@gmail.com>
2023-03-27 10:04:34 -0600

    cambios solicitados 27 marzo 2023

--- js/main.js
+++ js/main.js
@@ -13,14 +13,14 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () =>{
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
@@ -38,21 +38,21 @@ window.onscroll = () =>{
 };
 
 // Scroll reveal
-ScrollReveal({ 
+ScrollReveal({
     // reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .porfolio-box, .contact form', { origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .porfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // Multiple text
 const typed = new Typed('.multiple-text', {
-    strings: ['Diseñador', 'Publicista Profesional', 'Community Manager', 'Productor Ejecutivo'],
+    strings: ['Diseñador', 'Publicista Profesional', 'Community Manager', 'Productor Ejecutivo', 'Locutor'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
